628fda1d7bade56a00422f94b87c415b3f0f226f
gerywahyunugraha <gerywahyu@outlook.co.id>
2018-07-08 22:15:48 +0800

    Add Gson schema for location

---
+++ src/main/java/app/location/City.java
@@ -0,0 +1,58 @@
+package app.location;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class City {
+
+  @SerializedName("id")
+  @Expose
+  private Integer id;
+  @SerializedName("name")
+  @Expose
+  private String name;
+  @SerializedName("country")
+  @Expose
+  private String country;
+  @SerializedName("coord")
+  @Expose
+  private Coord coord;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public Coord getCoord() {
+    return coord;
+  }
+
+  public void setCoord(Coord coord) {
+    this.coord = coord;
+  }
+
+  @Override
+  public String toString() {
+    return name + " " + country;
+  }
+
+}
